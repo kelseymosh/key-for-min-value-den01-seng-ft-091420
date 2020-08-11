1997c43f82961a311746a4dafe3c220dcabe7ecc
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-11 20:15:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   empty_hash = nil
-  min_value = 0
+  min_value = 1
   name_hash.each do |name, value|
-    if min_value == 0 || value < min_value
+    if min_value == 1 || value < min_value
       min_value = value
       empty_hash = name
       end
